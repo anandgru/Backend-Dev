b983f1c80d30a8024db1271f0117875d2fcf2e65
anand Verma <anandverma6225@gmail.com>
2024-10-04 15:43:11 +0530

    Server Upload

--- FirstNode.js
+++ FirstNode.js
@@ -1,1 +1,17 @@
-console.log("Hello Word Hi");
+const http = require('http');
+
+const PORT = 4000;
+const name = 'Anand';
+
+const server = http.createServer((req, res) => {
+  console.log(`Accessed by: ${name}`);
+  
+  res.statusCode = 200; 
+  res.setHeader('Content-Type', 'text/plain'); 
+  res.end(`Hello! You accessed this server. Your name is ${name}.`); 
+});
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
