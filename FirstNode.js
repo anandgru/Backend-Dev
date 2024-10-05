4118300add3e0758b4cb0be271e848fabf002e24
anand Verma <anandverma6225@gmail.com>
2024-10-05 06:34:45 +0530

    SERVER req and res

--- FirstNode.js
+++ FirstNode.js
@@ -1,17 +1,35 @@
 const http = require('http');
 
-const PORT = 4000;
-const name = 'Anand';
-
+// Create the server
 const server = http.createServer((req, res) => {
-  console.log(`Accessed by: ${name}`);
-  
-  res.statusCode = 200; 
-  res.setHeader('Content-Type', 'text/plain'); 
-  res.end(`Hello! You accessed this server. Your name is ${name}.`); 
+  // Set the response headers
+  res.setHeader('Content-Type', 'text/plain');
+
+  // Check the request URL and return the appropriate response
+  switch (req.url) {
+    case '/home':
+      res.writeHead(200);
+      res.end('Welcome home');
+      break;
+    case '/about':
+      res.writeHead(200);
+      res.end('Welcome to About Us page');
+      break;
+    case '/node':
+      res.writeHead(200);
+      res.end('Welcome to my Node Js project');
+      break;
+    default:
+      res.writeHead(404);
+      res.end('Page not found');
+      break;
+  }
 });
+
+// Define the port to listen on
+const PORT = 3000;
 
 // Start the server
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
