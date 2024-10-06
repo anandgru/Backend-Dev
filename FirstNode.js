1be5bf1f9276ac031aaefc12e7e936dd9b6a76a8
anand Verma <anandverma6225@gmail.com>
2024-10-06 17:58:39 +0530

    CleanUp

--- FirstNode.js
+++ FirstNode.js
@@ -1,65 +1,6 @@
 const http = require("http");
-const fs = require("fs");
-var fileData;
-
-// Create the server
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    fs.readFile("msg.txt", "utf8", (err, data) => {
-      if (err) {
-        console.log("Error in readiing file: ", err);
-        return;
-      }
-      fileData = data;
-    });
-    res.write("<html><head><title>Your Msg</title></head>");
-    res.write("<body>");
-    res.write(`<pre>${fileData}</pre>`);
-    res.write(
-      '<form action="/message" method="POST"><input type="text" name="msg"><button type="submit">Send</button></body></html>'
-    );
-    return res.end();
-  }
-  // Set the response headers
-  res.setHeader("Content-Type", "text/html");
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("msg.txt", message);
-    });
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-  // Check the request URL and return the appropriate response
-  switch (req.url) {
-    case "/home":
-      res.writeHead(200);
-      res.end("Welcome home");
-      break;
-    case "/about":
-      res.writeHead(200);
-      res.end("Welcome to About Us page");
-      break;
-    case "/node":
-      res.writeHead(200);
-      res.end("Welcome to my Node Js project");
-      break;
-    default:
-      res.writeHead(404);
-      console.log(url);
-      res.end("Page not found");
-      break;
-  }
-});
-
+const cu = require("./cleanUp");
+const server=http.createServer(cu);
 // Define the port to listen on
 const PORT = 3000;
 
