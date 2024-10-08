85a4a051b512c27a6ad8728765e01c417e973fbd
anand Verma <anandverma6225@gmail.com>
2024-10-08 22:06:14 +0530

    Express 1

--- FirstNode.js
+++ FirstNode.js
@@ -1,9 +1,14 @@
 const http = require("http");
-const cu = require("./cleanUp");
-const server=http.createServer(cu);
-// Define the port to listen on
-const PORT = 3000;
-// Start the server
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const express = require("express");
+const app = express();
+app.use((req, res, next) => {
+  console.log("In the Middleware!");
+  next();
 });
+app.use((req, res, next) => {
+  console.log("In another Middleware!");
+  res.send('<h1>Hi!</h1>')
+});
+const server = http.createServer(app);
+// Define the port to listen on
+server.listen(3000);
