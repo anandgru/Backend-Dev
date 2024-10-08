b55d1d3d55332c5ff2c45e448d2e29cfed533ac4
anand Verma <anandverma6225@gmail.com>
2024-10-08 21:11:33 +0530

    Package

--- FirstNode.js
+++ FirstNode.js
@@ -3,7 +3,6 @@ const cu = require("./cleanUp");
 const server=http.createServer(cu);
 // Define the port to listen on
 const PORT = 3000;
-
 // Start the server
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
