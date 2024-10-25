b4613e86af209d476691e1cd5f4b096bebc60151
anand Verma <anandverma6225@gmail.com>
2024-10-25 11:29:21 +0530

    Routes with filter

--- FirstNode.js
+++ FirstNode.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
+app.use(express.json());
 const adminreq = require("./routes/admin");
 const shop = require("./routes/shop");
 const bodyParser = require("body-parser");
@@ -7,6 +10,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminreq);
 app.use("/shop", shop);
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found re</h1>");
+  res.status(404).send("<h1>Page not found</h1>");
 });
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/home", (req, res, next) => {
+router.get("/login", (req, res, next) => {
   res.send(
-    '<form action="/admin/office" method="POST"><input type="text" name="title"> <button type="submit"> Add Product</button></form>'
+    '<form action="/office" method="POST"> <input type="text" id="username" placeholder="Enter your username" required> <button type="submit"> LogIn</button></form>'
   );
 });
 router.post("/office", (req, res, next) => {
+  console.log("Hey Hmm Haa");
+  // const username = req.body.toString();//.substring(9, req.body.length - 3);
   console.log(req.body);
+  //localStorage.setItem("username", username);
   res.redirect("/shop");
 });
 module.exports = router;
